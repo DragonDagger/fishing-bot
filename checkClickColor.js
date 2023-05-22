f86648a715775447674b00c6715b3cbfeab09530
Dragon Dagger <zeddgrant@gmail.com>
2023-05-22 21:31:11 +1200

    improve clickcolor function

--- checkClickColor.js
+++ checkClickColor.js
@@ -1,55 +1,57 @@
+var robot = require("robotjs");
 // Checks the color of mouse clicks, red means that there is a fishing spot present. If color is not red, moves mouse to next location and repeats process.
-// Have archived these functions for now, until I can figure out a more consistent way to capture the color, as it stands currently I can not accurately determine color at the pixel I want.
 
-const newCheckFishingSpotColor = () => {
-  let level = 0;
-  const red = "ff0000";
-
-  sleep(4000);
-  robot.moveMouseSmooth(975, 678);
-  robot.mouseClick();
-  var img = robot.screen.capture();
-  var color = img.colorAt(974, 680.1);
-
-  if (color !== red && level === 0) {
-    level = 1;
-    console.log("first");
-  }
-
-  if (level === 1) {
-    console.log("second");
-  }
-
-  if (level === 1) {
-    console.log("second");
-  }
-
-  if (color === red) {
-    sleep(90000);
-  }
+const sleep = (ms) => {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
 };
 
-const oldCheckFishingSpotColor = () => {
-  const red = "ff0000";
-  sleep(4000);
+function checkClickColor(x, y) {
+  const activeClickColor = ["ff0000"];
 
-  robot.moveMouseSmooth(975, 678);
+  robot.moveMouseSmooth(x, y);
   robot.mouseClick();
   robot.mouseClick();
-  var img = robot.screen.capture();
-  var color = img.colorAt(974, 679.1);
+
+  // taking a screenshot
+  var img = robot.screen.capture(0, 0, 1920, 1080);
+  var samePixel = img.colorAt(x, y);
+  var secondcolor = img.colorAt(x - 2, y + 2);
+  var thirdcolor = img.colorAt(x, y + 1.1);
+  var fourthcolor = img.colorAt(x - 1, y);
+  var fifthcolor = img.colorAt(x + 2, y + 1.1);
+
+  if (activeClickColor.includes(samePixel)) {
+    console.log("Active fishing spot found on samePixel check");
+    return true;
+  }
+  if (activeClickColor.includes(secondcolor)) {
+    console.log("Active fishing spot found on second check");
+    return true;
+  }
+  if (activeClickColor.includes(thirdcolor)) {
+    console.log("Active fishing spot found on third check");
+    return true;
+  }
+  if (activeClickColor.includes(fourthcolor)) {
+    console.log("Active fishing spot found on fourth check");
+    return true;
+  }
+  if (activeClickColor.includes(fifthcolor)) {
+    console.log("Active fishing spot found on fifth check");
+    return true;
+  } else {
+    console.log("no red here.");
+    return false;
+  }
+}
+
+const checkFishingSpot = () => {
+  const firstSpot = checkClickColor(975, 678);
   sleep(2000);
+  if (firstSpot === false) {
+    const secondSpot = checkClickColor(850, 685);
 
-  if (color !== red) {
-    console.log(color);
-    sleep(2000);
-    robot.moveMouseSmooth(850, 685);
-    robot.mouseClick();
-    robot.mouseClick();
-    var img = robot.screen.capture();
-    var color = img.colorAt(849, 686.1);
-
-    if (color === red) {
+    if (secondSpot === true) {
       sleep(90000);
       robot.moveMouseSmooth(1050, 505);
       robot.mouseClick();
@@ -57,18 +59,15 @@ const oldCheckFishingSpotColor = () => {
       checkFishingSpot();
     }
 
-    if (color !== red) {
-      console.log(color);
+    if (secondSpot === false) {
+      sleep(2000);
       robot.moveMouseSmooth(1050, 590);
       robot.mouseClick();
       sleep(4000);
-      robot.moveMouseSmooth(1200, 515);
-      robot.mouseClick();
-      robot.mouseClick();
-      var img = robot.screen.capture();
-      var color = img.colorAt(1199, 516.1);
+      const thirdSpot = checkClickColor(1200, 515);
+      sleep(2000);
 
-      if (color === red) {
+      if (thirdSpot === true) {
         sleep(90000);
         robot.moveMouseSmooth(850, 755);
         robot.mouseClick();
@@ -76,8 +75,7 @@ const oldCheckFishingSpotColor = () => {
         checkFishingSpot();
       }
 
-      if (color !== red) {
-        console.log(color);
+      if (thirdSpot === false) {
         robot.moveMouseSmooth(850, 755);
         robot.mouseClick();
         sleep(2000);
@@ -85,29 +83,17 @@ const oldCheckFishingSpotColor = () => {
       }
     }
   } else {
-    console.log(`color was ${color}`);
+    console.log(`inside the else statement`);
     sleep(90000);
     console.log("finished sleeping...");
   }
 };
 
-function testScreenCapture() {
-  // const activeParticleColors = [
-  //   "6695dc",
-  //   "82afe3",
-  //   "5d6c87",
-  //   "f3e9f1",
-  //   "99bde7",
-  //   "aacaec",
-  //   "bdcaec",
-  //   "739bd3",
-  // ];
-  sleep(4000);
-  robot.moveMouseSmooth(975, 678);
-  robot.mouseClick();
-  robot.mouseClick();
-  // taking a screenshot
-  var img = robot.screen.capture(0, 0, 1920, 1080);
-  var color = img.colorAt(972, 679.1);
-  console.log(`the color is: ${color}`);
-}
+// const zoggy = testScreenCapture();
+
+// if (zoggy === true) {
+//   console.log("TRUE");
+// }
+// if (zoggy === false) {
+//   console.log("False");
+// }
